b03b3678bb365d6e58659ed3d8a125c14c2a0e5a
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-06-07 06:48:13 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello")  //only supports in browser
+
+let name = "shubham"
+let age = 19
+let isLoggedIn = false
+
+//! primitive data types
+//number => 2power53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>not assigned value till now
+//symbol => for unique value
+
+
+console.log(typeof null);  //object 
+console.log(typeof undefined);  //undefined
